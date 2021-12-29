3990e1e2956f977cbd8b335b54ec3f33686f9afd
alenwenish <45667479+alenwenish@users.noreply.github.com>
2021-12-29 20:30:22 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+var loader = document.getElementById("preloader");
+window.addEventListener("load",function(){
+    loader.style.display = "none";
+})
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+let themeToggler = document.querySelector('#theme-toggler');
+
+themeToggler.onclick = () =>{
+    themeToggler.classList.toggle('fa-sun');
+    if(themeToggler.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+    }
+    else
+    {
+        document.body.classList.remove('active');
+    }
+}
+document.querySelector('.switcher-btn').onclick = () =>{
+    document.querySelector('.color-switcher').classList.toggle('active');
+};
+
+var typed = new Typed('.typing-text', {
+    strings : ['Student','Tech Enthusiast','Developer'],
+    loop : true,
+    typeSpeed : 150
+});
+
+let themeButtons = document.querySelectorAll('.theme-buttons');
+
+themeButtons.forEach(color =>{
+    color.addEventListener('click', () =>{
+        let dataColor = color.getAttribute('data-color');
+        document.querySelector(':root').style.setProperty('--main-color', dataColor);
+    })
+})
