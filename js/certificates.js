b4fed1db1eefb4c07921e7db9797c53dcd64df32
alenwenish <alenwenish@gmail.com>
2022-09-04 22:02:45 +0530

    Updated website

---
+++ js/certificates.js
@@ -0,0 +1,32 @@
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+let themeToggler = document.querySelector('#theme-toggler');
+
+themeToggler.onclick = () =>{
+    themeToggler.classList.toggle('fa-sun');
+    if(themeToggler.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+    }
+    else
+    {
+        document.body.classList.remove('active');
+    }
+}
+document.querySelector('.switcher-btn').onclick = () =>{
+    document.querySelector('.color-switcher').classList.toggle('active');
+};
+
+let themeButtons = document.querySelectorAll('.theme-buttons');
+
+themeButtons.forEach(color =>{
+    color.addEventListener('click', () =>{
+        let dataColor = color.getAttribute('data-color');
+        document.querySelector(':root').style.setProperty('--main-color', dataColor);
+    })
+})
